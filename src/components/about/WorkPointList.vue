2e98c5e05f103cc0f5b785a8c4882659bfa7484c
naseulgee <thwndglgkwk@naver.com>
2024-12-06 11:30:17 +0900

    워크 포인트 백드롭 필터 추가

--- src/components/about/WorkPointList.vue
+++ src/components/about/WorkPointList.vue
@@ -5,9 +5,9 @@
         Work Point
     </h2>
     <ul
-        class="container d-flex flex-wrap align-content-start justify-content-center align-items-start"
+        class="container d-flex flex-wrap align-content-start justify-content-center align-items-stretch"
         :class="[...classObj.containerClass, { iconOff }]">
-        <li>
+        <li class="px-1 py-3 rounded-3 overflow-hidden">
             <span :class="[...classObj.iconClass]">
                 <font-awesome-icon
                     class="icon"
@@ -17,11 +17,11 @@
             <strong :class="['subtitle', ...classObj.subtitleClass]">
                 다양한 환경
             </strong>
-            <p :class="['dec', ...classObj.decClass]">
+            <p :class="['dec m-0', ...classObj.decClass]">
                 환경에 영향을 받지 않고 동일한 정보를 전달하도록 개발합니다.
             </p>
         </li>
-        <li>
+        <li class="px-1 py-3 rounded-3 overflow-hidden">
             <span :class="[...classObj.iconClass]">
                 <font-awesome-icon
                     class="icon"
@@ -31,11 +31,11 @@
             <strong :class="['subtitle', ...classObj.subtitleClass]">
                 속도 개선
             </strong>
-            <p :class="['dec', ...classObj.decClass]">
+            <p :class="['dec m-0', ...classObj.decClass]">
                 사용자가 사이트에 집중할 수 있도록 속도 개선에 대해 끊임 없이 고민하며 불필요한 인터렉션을 최소화 하기 위해 노력합니다.
             </p>
         </li>
-        <li>
+        <li class="px-1 py-3 rounded-3 overflow-hidden">
             <span :class="[...classObj.iconClass]">
                 <font-awesome-icon
                     class="icon"
@@ -45,7 +45,7 @@
             <strong :class="['subtitle', ...classObj.subtitleClass]">
                 데이터 기반
             </strong>
-            <p :class="['dec', ...classObj.decClass]">
+            <p :class="['dec m-0', ...classObj.decClass]">
                 사용자의 행동 경로와 A/B 테스트 등의 통계를 기반으로 개발하여 이탈률을 감소시킵니다.
             </p>
         </li>
@@ -82,6 +82,8 @@ export default {
         flex: 1;
         flex-shrink: 0;
         min-width: 200px;
+        background-color: rgba(var(--bs-white-rgb), 0.05);
+        backdrop-filter: blur(20px);
         .icon{
             display: inline-block;
             color: $primary;
