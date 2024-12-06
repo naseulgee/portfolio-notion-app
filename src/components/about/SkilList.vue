1f14052be93ad04e7940d80117614d518c6e82b2
naseulgee <thwndglgkwk@naver.com>
2024-12-06 10:21:54 +0900

    스킬 리스트 UI 변경

--- src/components/about/SkilList.vue
+++ src/components/about/SkilList.vue
@@ -6,61 +6,48 @@
         @ob-callback="obCallback"
         class="text-dark">
         <template
-            v-for="(stack, i) of stackList"
+            v-for="stack of stackList"
             :key="stack.name">
             <li
-                v-if="(i - 1) % 2 == 0"
-                class="floating-wrap d-flex flex-wrap flex-md-nowrap justify-content-between align-items-end mb-2 mb-md-3">
-                <div
-                    v-if="stackList[(i - 1)]"
-                    class="floating-box mb-2 mb-md-0 rounded-3"
-                    :class="{ 'small-box': (i - 1) % 4 == 0 }"
-                    :style="{ backgroundColor: randomColor() }">
-                    <h2 class="p-2 border-dark fs-4">
-                        {{ stackList[(i - 1)].name }}
-                    </h2>
-                    <ul class="d-flex flex-wrap justify-content-center align-items-start p-1 p-md-2 text-center">
-                        <li
-                            v-for="el in stackList[(i - 1)].options"
-                            :key="el.id"
-                            class="mw-100">
-                            <div class="icon mx-auto">
-                                <NotionObj :prop="el.icon" />
-                            </div>
-                            <br />
-                            {{ el.name.split(' ')[0] }}
-                            <template v-if="el.name.split(' ')[1]">
-                                <br />
-                                {{ el.name.split(' ')[1] }}
-                            </template>
-                        </li>
-                    </ul>
+                class="floating-wrap w-100 mb-2 rounded-3 position-relative text-center"
+                :style="{ backgroundColor: randomColor() }">
+                <h2 class="m-0 p-2 fs-4">
+                    {{ stack.name }}
+                </h2>
+                <div class="line position-relative">
+                    <div class="circle rounded-circle position-absolute top-50 start-0 translate-middle bg-white"></div>
+                    <div class="circle rounded-circle position-absolute top-50 end-0 bg-white"></div>
+                    <svg
+                        class="d-block"
+                        height="5"
+                        width="100%"
+                        viewBox="0 0 100 100"
+                        preserveAspectRatio="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M0 50 L100 50"
+                            stroke="var(--bs-body-bg)"
+                            stroke-width="5"
+                            stroke-linecap="round"
+                            stroke-dasharray="10, 15"
+                            stroke-dashoffset="-10"
+                            vector-effect="non-scaling-stroke" />
+                    </svg>
                 </div>
-                <div
-                    v-if="stackList[i]"
-                    class="floating-box rounded-3"
-                    :class="{ 'small-box': (i - 1) % 4 != 0 }"
-                    :style="{ backgroundColor: randomColor() }">
-                    <h2 class="p-2 border-dark fs-4">
-                        {{ stackList[i].name }}
-                    </h2>
-                    <ul class="d-flex flex-wrap justify-content-center align-items-start p-1 p-md-2 text-center">
-                        <li
-                            v-for="el in stackList[i].options"
-                            :key="el.id"
-                            class="mw-100">
-                            <div class="icon mx-auto">
-                                <NotionObj :prop="el.icon" />
-                            </div>
+                <ul class="d-flex flex-wrap align-items-start p-2 text-center">
+                    <li
+                        v-for="option in stack.options"
+                        :key="option.id">
+                        <div class="icon mx-auto mb-1">
+                            <NotionObj :prop="option.icon" />
+                        </div>
+                        {{ option.name.split(' ')[0] }}
+                        <template v-if="option.name.split(' ')[1]">
                             <br />
-                            {{ el.name.split(' ')[0] }}
-                            <template v-if="el.name.split(' ')[1]">
-                                <br />
-                                {{ el.name.split(' ')[1] }}
-                            </template>
-                        </li>
-                    </ul>
-                </div>
+                            {{ option.name.split(' ')[1] }}
+                        </template>
+                    </li>
+                </ul>
             </li>
         </template>
     </FloatingWrap>
@@ -109,70 +96,59 @@ export default {
 
 <style lang="scss" scoped>
 .floating-wrap{
-    .floating-box{
-        --width: #{'min(750px, (50vw - ' + $spacer + '))'};
-        width: var(--width);
-        opacity: 0;
-        transition: $transition-base;
-        font-size: 1.5rem;
-        &.small-box{ // 큰 사이즈
-            --width: #{'min(400px, (40vw - ' + $spacer + '))'};
+    opacity: 0;
+    transition: $transition-base;
+    font-size: 1.5rem;
+    .line{
+        .circle{
+            width: 30px;
+            aspect-ratio: 1;
+            &.end-0{
+                transform: translate(50%, -50%);
+            }
         }
-        &:nth-child(odd){
-            transform: translateX(-100%) !important;
-        }
-        &:nth-child(even){
-            transform: translateX(100%) !important;
-        }
-        h2{
-            border-bottom: 1px dashed;
-        }
-        ul{
-            gap: $spacer * 0.5;
-            li{
-                font-size: 0.5em;
-                .icon{
-                    width: 70px;
-                    height: 70px;
-                    line-height: 1;
-                }
+    }
+    ul{
+        justify-content: center;
+        gap: $spacer * 0.5;
+        li{
+            max-width: 100%;
+            font-size: 0.5em;
+            .icon{
+                width: 70px;
+                max-width: 100%;
+                aspect-ratio: 1;
+                line-height: 1;
+                filter: drop-shadow(1px 1px 1px #00000050);
             }
         }
     }
     &.on{
-        .floating-box{
-            opacity: 1;
-            transform: none !important;
-        }
+        opacity: 1;
     }
-}
-/* [Tab] =================== */
-@include media-breakpoint-down(xl){
-    .floating-wrap{
-        .floating-box{
-            --width: 50%;
-            &.small-box{ // 작은 사이즈
-                --width: #{'calc(50% - ' + $spacer * 0.5 + ')'};
-            }
-        }
+    &::after{
+        display: block;
+        content: '';
+        width: 95%;
+        height: 20px;
+        position: absolute;
+        bottom: 0;
+        left: 50%;
+        transform: translate(-50%, 50%);
+        filter: blur(10px) brightness(0.8);
+        background: inherit;
+        z-index: -1;
     }
 }
 /* [MO] =================== */
 @include media-breakpoint-down(md){
     .floating-wrap{
-        .floating-box{
-            --width: 100%;
-            &.small-box{ // 작은 사이즈
-                --width: 100%;
-            }
-            ul{
-                li{
-                    font-size: 0.8em;
-                    .icon{
-                        width: 100%;
-                        height: auto;
-                    }
-                }
+        ul{
+            justify-content: space-between;
+            gap: 5px;
+            li{
+                max-width: 45%;
+                font-size: 10px;
             }
         }
     }
